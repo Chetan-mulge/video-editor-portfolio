ccf7541a149ebf2ca8e0fe7432220bf10ad78ee6
Chetan <chetan@Chetans-MacBook-Air.local>
2026-07-14 17:57:25 +0530

    Fix production build

--- app/components/VisualAtmosphere.tsx
+++ app/components/VisualAtmosphere.tsx
@@ -1,17 +1,20 @@
 "use client";
+import * as THREE from "three";
 import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 function Particles() {
-  const ref = useRef<any>();
+  const ref = useRef<THREE.Points>(null);
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.5 });
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+  if (!ref.current) return;
+
+  ref.current.rotation.x -= delta / 10;
+  ref.current.rotation.y -= delta / 15;
+});
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
